98e5bcc2c51a84a556c85e7ab9a1c2b291082cf6
Asaad27 <asaad.belarbi@grenoble-inp.org>
2022-01-07 01:38:10 +0100

    file gen

--- fileGen.go
+++ fileGen.go
@@ -9,11 +9,9 @@ import (
 	"strconv"
 )
 
-//input : size of piece in KB, number of pieces, begin-end
-//1000 * 256KB = 25600 KB = 256 MB
 var sizeOfPiece = int64(256 * 1024)
 var numberOfPieces = int64(1000)
-var sizeOfFile = numberOfPieces * sizeOfPiece
+var sizeOfFile = 0
 
 func makeFile(fileName string, numPieces int64, offset int64) {
 
@@ -33,14 +31,14 @@ func makeFile(fileName string, numPieces int64, offset int64) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := f.Truncate(sizeOfFile); err != nil {
+	if err := f.Truncate(int64(sizeOfFile)); err != nil {
 		log.Fatal(err)
 	}
 
 	_, err = f.WriteAt(bytes.Repeat([]byte("a"), int(numPieces*sizeOfPiece)), offset*sizeOfPiece)
 
-	_, err = f.WriteAt(bytes.Repeat([]byte("b"), int(offset*sizeOfPiece)), 0)
-	_, err = f.WriteAt(bytes.Repeat([]byte("b"), int((numberOfPieces-(offset+numPieces))*sizeOfPiece)), (offset+numPieces)*sizeOfPiece)
+	_, err = f.WriteAt(bytes.Repeat([]byte("x"), int(offset*sizeOfPiece)), 0)
+	_, err = f.WriteAt(bytes.Repeat([]byte("x"), int((numberOfPieces-(offset+numPieces))*sizeOfPiece)), (offset+numPieces)*sizeOfPiece)
 	if err != nil {
 		log.Fatalf("failed to write")
 	}
@@ -50,11 +48,32 @@ func main() {
 	fileName := os.Args[1]
 	from, _ := strconv.Atoi(os.Args[2])
 	to, _ := strconv.Atoi(os.Args[3])
+	if len(os.Args) > 4{
+		var err error
+		var argValue int
+		argValue, err = strconv.Atoi(os.Args[4])
+		numberOfPieces = int64(argValue)
+		if err != nil {
+			log.Fatalf("error parsing last argument, verify number of pieces")
+		}
+	}
+	if len(os.Args) > 5{
+		var err error
+		var argValue int
+		argValue, err = strconv.Atoi(os.Args[5])
+		sizeOfPiece = int64(argValue)
+		if err != nil{
+			log.Fatalf("error parsing last argument")
+		}
+	}
+	sizeOfFile = int(sizeOfPiece * numberOfPieces)
 	to = int(math.Min(float64(numberOfPieces), float64(to)))
 	from = int(math.Max(1, float64(from)))
 	numberOfPiecesToWrite := to - from + 1
 	offset := from
-	fmt.Println("fileName : " + fileName + " nofPieces : " + strconv.FormatInt(int64(numberOfPiecesToWrite), 10) + " offset: " + strconv.FormatInt(int64(offset), 10))
+	fmt.Printf("size of piece : %v\t number of pieces to write: %v\t size of file : %v\n",sizeOfPiece, numberOfPiecesToWrite, sizeOfFile )
+
+	fmt.Println("fileName : " + fileName + " \tPieces: " + strconv.FormatInt(int64(from), 10) + " -> " + strconv.FormatInt(int64(to), 10))
 	makeFile(fileName, int64(numberOfPiecesToWrite), int64(offset))
 
 }
